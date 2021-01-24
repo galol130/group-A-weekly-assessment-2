9eb88a8fdf40af609bcddac4f313a15731fc51a9
Galo García Pugliese <galo130@gmail.com>
2021-01-24 14:52:59 +0100

    MenÂnu modified and input command validated for every case

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -1,27 +1,59 @@
 package com.ironhack;
 
+import com.ironhack.classes.Account;
 import com.ironhack.classes.Input;
+import com.ironhack.classes.Lead;
+import com.ironhack.classes.Opportunity;
 import com.ironhack.styles.ConsoleColors;
 import com.ironhack.styles.Start;
 
 public class Main {
     public static void main(String[] args) {
-//        Print the system header:
+//      Print the system header:
         Start.printStart();
-//        Display initial menu:
-        System.out.println("\nWelcome, please write one of the following:\n" +
-                ConsoleColors.BLUE + "Leads\n" + ConsoleColors.WHITE_BOLD +
-                "\tWrite: 'New lead' to enter a new lead record.\n" +
-                "\tWrite: 'Show leads' to view all current leads.\n" +
-                "\tWrite: 'Lookup lead + [id]' to search for a specific lead.\n" +
-                ConsoleColors.BLUE + "Opportunities\n" + ConsoleColors.WHITE_BOLD +
-                "\tWrite: 'Convert + [id]' to convert a lead into an opportunity.\n" +
-                "\tWrite: 'Change status' to change an opportunity status.\n" +
-                ConsoleColors.BLUE + "Accounts\n" + ConsoleColors.WHITE_BOLD +
-                "\tWrite: 'Show accounts' to view all current accounts.\n" +
-                "\n" +
-                "\tWrite: 'Quit system' to exit.\n");
-//        Call the getCommand method that validates menu input and calls proper methods accordingly.
-        Input.getCommand();
+//      Print initial menu (designers gave us different options):
+        Start.printMenuV2();
+//      Call the getCommand method that validates menu input and calls proper methods accordingly.
+        //Input.getCommand();
+        String[] command = Input.getValidCommand();
+
+        //The command method is called
+        switch (command[0]){
+            case "new":
+                //Método a ser creado
+                //Lead.createLead();
+                break;
+            case "show": //two options
+                //if(command[1].equals("lead"))
+                    //Método a ser creado
+                    //Lead.showLeads();
+                //else
+                    //Método a ser creado
+                    //Account.showAccounts();
+                break;
+            case "lookup":
+                //Método a ser creado
+                //Lead.lookupById(Integer.parseInt(command[2]));
+                break;
+            case "convert":
+                convert(Integer.parseInt(command[1]));
+                break;
+            case "change":
+                //Método a ser creado
+                //Opportunity.changeStatus(Integer.parseInt(command[2]));
+                break;
+            case "exit":
+                System.exit(1);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void convert(Integer id){
+        //Se debe llamar al método de crear Opportunity
+        //Luego llamar al método de crear Contact
+        //Luego llamar al método de crear Account
+        //Por último eliminar el lead del id que llega a este método
     }
 }
--- src/main/java/com/ironhack/classes/Input.java
+++ src/main/java/com/ironhack/classes/Input.java
@@ -1,56 +1,99 @@
 package com.ironhack.classes;
 
+import com.ironhack.styles.ConsoleColors;
+
 import java.util.Scanner;
 
 public class Input {
 
-	//Gets a command from user input.
-	public static void getCommand() {
-		Scanner		scanner = new Scanner(System.in);
-		String		command;
-		String[]	commandSplit;
-		int			id;
+    //Gets a command from user input.
+    public static void getCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        String[] commandSplit;
+        int id;
 
-		while (true) {
-			System.out.println("Write a command:");
-			command = scanner.nextLine().toLowerCase().trim();
-			commandSplit = command.split(" ");
-			if (commandSplit.length != 2) {
-				System.out.println("Command format must be: [command] [id].");
-				continue;
-			}
-			try {
-				if (commandSplit[0].equals("convert")) {
-					id = getInputId(commandSplit[1]);
-					////////Call to convert method with previous id;
-					break;
-				}
-				else {
-					System.out.println("Not a valid command.");
-				}
-			}
-			catch (Exception e) {
-				System.out.println("Something was wrong.");
-			}
-		}
-	}
+        while (true) {
+            System.out.println("Write a command:");
+            command = scanner.nextLine().toLowerCase().trim();
+            commandSplit = command.split(" ");
+            if (commandSplit.length != 2) {
+                System.out.println("Command format must be: [command] [id].");
+                continue;
+            }
+            try {
+                if (commandSplit[0].equals("convert")) {
+                    id = getInputId(commandSplit[1]);
+                    ////////Call to convert method with previous id;
+                    break;
+                } else {
+                    System.out.println("Not a valid command.");
+                }
+            } catch (Exception e) {
+                System.out.println("Something was wrong.");
+            }
+        }
+    }
 
-	public static int getInputId(String idStr) {
-		int		result = 0;
+    public static int getInputId(String idStr) {
+        int result = 0;
 
-		try {
-			result = Integer.parseInt(idStr);
-		}
-		catch (NumberFormatException e) {
-			System.out.println("Id must be a number.");
-			idStr = "";
-		}
-		catch (Exception e) {
-			System.out.println("Something was wrong.");
-			idStr = "";
-		}
-		return result;
-	}
+        try {
+            result = Integer.parseInt(idStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Id must be a number.");
+            idStr = "";
+        } catch (Exception e) {
+            System.out.println("Something was wrong.");
+            idStr = "";
+        }
+        return result;
+    }
+
+    //Validates a command typed by a user, returning the command as an array of strings only if it's a valid one.
+    public static String[] getValidCommand() {
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        String[] commandSplit;
+
+        while (true) {
+            System.out.println("Type a command:");
+            command = scanner.nextLine().toLowerCase().trim();
+            commandSplit = command.split(" ");
+            //Validates if the command is ok and doesn't require an [id]
+            if (command.equals("new lead")
+                    || command.equals("show leads")
+                    || command.equals("show accounts")
+                    || command.equals("exit")) {
+                return commandSplit;
+            } else if (commandSplit[0].equals("convert")) {
+                //Here must validate id is a number
+                if (validIdFormat(commandSplit[1]))
+                    return commandSplit;
+            } else if (commandSplit[0].concat(commandSplit[1]).equals("lookuplead")) {
+                //Here must validate id is a number;
+                if (validIdFormat(commandSplit[2]))
+                    return commandSplit;
+            } else if (commandSplit[0].concat(commandSplit[1]).equals("changestatus")) {
+                //Here must validate id is a number;
+                if (validIdFormat(commandSplit[2]))
+                    return commandSplit;
+            } else {
+                //command is not correct
+                System.out.println(ConsoleColors.RED_BOLD + "The command type does not match any available one. Try again!");
+            }
+        }
+    }
+
+    public static boolean validIdFormat(String stringId) {
+        try {
+            Integer.parseInt(stringId);
+            return true;
+        } catch (Exception e) {
+            System.out.println(ConsoleColors.RED_BOLD + "Error: An integer was expected as an Id");
+        }
+        return false;
+    }
 
 //	public static int getInputNumber(int min, int max) {
 //		Scanner scanner = new Scanner(System.in);
