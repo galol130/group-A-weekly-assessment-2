fb844899728af0fb68248d6f5a14f989a378e9e9
dani <danijuancar@gmail.com>
2021-01-28 23:11:21 +0100

    Addind pom dependency for tests plus the class for data

---
+++ src/test/java/com/ironhack/data/DataTest.java
@@ -0,0 +1,46 @@
+package com.ironhack.data;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    void showLeads() {
+    }
+
+    @Test
+    void lookUpLead() {
+    }
+
+    @Test
+    void deleteLead() {
+    }
+
+    @Test
+    void changeOpportunityStatus() {
+    }
+
+    @Test
+    void getOpportunityById() {
+    }
+
+    @Test
+    void showAccounts() {
+    }
+
+    @Test
+    void showOpportunities() {
+    }
+}
